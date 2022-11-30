1247c5dfc325c8376e530502cffde2604373956c
Liliana <unicred@MacBook-Pro-de-Liliana-Abdala.local>
2022-11-30 14:25:53 -0300

    Tela login realizada

---
+++ Banco Inter 1.0/SecondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SecondViewController.swift
+//  Banco Inter 1.0
+//
+//  Created by Liliana Porto Abdala on 30/11/22.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var Loginemail: UIView!
+    
+    @IBOutlet var Loginsenha: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+    }
---
+++ Banco Inter 1.0/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Banco Inter 1.0
+//
+//  Created by Liliana Porto Abdala on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
